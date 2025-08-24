31fbd9ca56edfe088b3b8db32776a99fe9a26ec7
ArjanJhamesM <arjanjhamesm@gmail.com>
2025-08-25 00:04:08 +0800

    Implement quiz creation

--- main.cpp
+++ main.cpp
@@ -10,16 +10,27 @@ using std::endl;
 using std::string;
 using std::vector;
 
-class CreatedQuestions {
+class Questionnaire {
     private:
-        struct questionTemplate
-        {
-            string question, choiceA, choiceB, choiceC, choiceD;
-            char correctChoice, userChoice;
-            bool isTimed;
+        struct questionTemplate { // encapsulation
+            string question = "", choiceA = "", choiceB = "", choiceC = "", choiceD = "";
+            char correctChoice = '\0', selectedAnswer = '\0';
+            bool isTimed = false;
+            int durationSeconds = 0;
         };
 
-        vector<questionTemplate> createdQuestions;
+    public:
+        using qTemplate = questionTemplate;
+
+        questionTemplate getQuestionTemplate() {
+            questionTemplate qTemplate;
+            return qTemplate;
+        }
+};
+
+class CreatedQuestions {
+    private:
+        vector<Questionnaire::qTemplate> createdQuestions;
 
     public:
         void addToCreatedQuestions(const string &userQuestion,
@@ -28,52 +39,183 @@ class CreatedQuestions {
                                    const string &userChoiceC,
                                    const string &userChoiceD,
                                    const char userCorrectChoice,
-                                   const bool userIsTimed)
+                                   const char userSelectedAnswer,
+                                   const bool userIsTimed,
+                                   const int userDurationSeconds)
         {
-            questionTemplate qTemplate;
+            Questionnaire questionnaire;
+            Questionnaire::qTemplate questionnaireCopy = questionnaire.getQuestionTemplate();
 
-            qTemplate.question = userQuestion;
-            qTemplate.choiceA = userChoiceA;
-            qTemplate.choiceB = userChoiceB;
-            qTemplate.choiceC = userChoiceC;
-            qTemplate.choiceD = userChoiceD;
-            qTemplate.correctChoice = userCorrectChoice;
-            qTemplate.isTimed = userIsTimed;
+            questionnaireCopy.question = userQuestion;
+            questionnaireCopy.choiceA = userChoiceA;
+            questionnaireCopy.choiceB = userChoiceB;
+            questionnaireCopy.choiceC = userChoiceC;
+            questionnaireCopy.choiceD = userChoiceD;
+            questionnaireCopy.correctChoice = userCorrectChoice;
+            questionnaireCopy.selectedAnswer = userSelectedAnswer;
+            questionnaireCopy.isTimed = userIsTimed;
+            questionnaireCopy.durationSeconds = userDurationSeconds;
 
-            createdQuestions.push_back(qTemplate);
+            createdQuestions.push_back(questionnaireCopy);
         }
 
-        string getVectorQuestion() { // TEMP: getter for debug
-            questionTemplate qTemplate;
-            return createdQuestions[0].question;
-        }
+        #pragma region VectorDataDebug
+            string getVectorQuestion() { // TEMP: getter for debug
+                return createdQuestions[0].question;
+            }
+        #pragma endregion
 };
 
-void collectUserInput(const string outputText, string &tempVariable) { // WIP
-    cout << outputText << ": " << endl;
-    getline(cin, tempVariable); // TODO: validate type
+char ConvertLetterStringToChar(string input) {
+    if (!input.empty()) {
+        char convertedLetter = input.front();
+        return convertedLetter;
+    }
+    else {
+        return '\0';
+    }
 }
 
-int main()
-{
-    // TODO: move to new class for quiz creation
-    string question = "", choiceA = "", choiceB = "", choiceC = "", choiceD = "";
-    char correctChoice = '\0', userChoice = '\0';
-    bool isTimed = false;
+class QuizCreation {
+    private:
+        // TODO: separate into another class
+        Questionnaire questionnaire;
+        Questionnaire::qTemplate inputQuestionData = questionnaire.getQuestionTemplate();
 
-    collectUserInput("Enter your question", question); // WIP: implementation
+        int numberOfQuestionsToCreate = 0;
+        int numberOfCreatedQuestions = 0;
 
-    cout << "Local Question variable data: " << question << endl;
+        string inputData;
 
-    // TODO: make repeatable for new questions to be created
-    CreatedQuestions cQuestions;
+        int displayNumberForNewQuestion() {
+            return numberOfCreatedQuestions + 1;
+        }
 
-    cQuestions.addToCreatedQuestions(question, choiceA, choiceB, choiceC, choiceD, correctChoice, isTimed);
-    // TODO: wipe user input variables after for new questions
+        bool hasMoreQuestionsToCreate() {
+            return (numberOfCreatedQuestions < numberOfQuestionsToCreate);
+        }
 
-    // TEMP: debug output of vector data
-    cout << "Vector Question data: " << cQuestions.getVectorQuestion() << endl;
-    cin;
+        bool askTotalQuestionsCount()
+        {
+            cout << "How many questions would you like to create?" << endl;
+
+            string input = "";
+            getline(cin, input);
+
+            try
+            {
+                int stringInputToInt = std::stoi(input);
+
+                if (stringInputToInt > 0) {
+                    numberOfQuestionsToCreate = stringInputToInt;
+                }
+                else {
+                    cout << "Warning: Number input cannot be zero!" << endl;
+                    return false;
+                }
+
+                cout << "DEBUG | Retrieved user input for numberOfQuestionsToCreate: " << numberOfQuestionsToCreate << endl; // DEBUG
+                return true;
+            }
+            catch (const std::invalid_argument &e)
+            {
+                cout << "Warning: Non-numerical input entered!" << endl;
+                return false;
+            }
+            catch (const std::out_of_range &e)
+            {
+                cout << "Warning: Number input too large!" << endl;
+                return false;
+            }
+        }
+
+        void promptTotalNumberUntilSuccess() {
+            while (!askTotalQuestionsCount()) {
+                cout << "Warning: Invalid input!" << endl;
+            }
+        }
+
+        template <typename T>
+        bool promptQuestionDetails(const string &inputSelection, const string &promptText, T &questionDataField) { // TODO: track current question number
+            cout << promptText << endl;
+            getline(cin, inputData);
+
+            if (inputSelection == "Question")
+            {
+                if constexpr (std::is_same_v<T, string>)
+                {
+                    if (inputData.empty())
+                    {
+                        cout << "Warning: No Question specified!" << endl; // TODO: abstract input selection in this message
+                        return false;
+                    }
+
+                    cout << "DEBUG | Question selected" << endl; // DEBUG
+                    questionDataField = inputData;
+                    cout << "DEBUG | Retrieved user input: " << questionDataField << endl; // DEBUG
+                    return true;
+                }
+            }
+
+            if (inputSelection == "Correct Choice") {
+                if constexpr (std::is_same_v<T, char>)
+                {
+                    cout << "DEBUG | Correct Choice selected" << endl; // DEBUG
+                    char charConversionResult = ConvertLetterStringToChar(inputData);
+
+                    if (charConversionResult == '\0')
+                    {
+                        cout << "Warning: No Correct Choice specified!" << endl;
+                        return false;
+                    }
+
+                    questionDataField = charConversionResult;
+                    cout << "DEBUG | Retrieved user input: " << questionDataField << endl; // DEBUG
+                    return true;
+                }
+            }
+            // TODO: if-check for all int input selections
+
+            return false;
+        }
+
+        template <typename T>
+        void promptDetailsUntilSuccess(const string inputSelection, const string promptText, T &questionDataField) {
+            while (!promptQuestionDetails(inputSelection, promptText, questionDataField)) {
+                cout << "Warning: Invalid input!" << endl;
+            }
+        }
+
+        void buildQuestionUsingDetails() {
+            string questionPrompt = "Enter question " + std::to_string(displayNumberForNewQuestion()) + ":";
+            promptDetailsUntilSuccess("Question", questionPrompt, inputQuestionData.question);
+            promptDetailsUntilSuccess("Correct Choice", "Enter the correct choice: ", inputQuestionData.correctChoice);
+            numberOfCreatedQuestions++;
+            cout << "DEBUG | Retrieved number of questions created: " << numberOfCreatedQuestions << endl; // DEBUG
+            return;
+        }
+
+    public:
+        void runQuizCreation() {
+            promptTotalNumberUntilSuccess();
+
+            while (hasMoreQuestionsToCreate()) {
+                buildQuestionUsingDetails();
+            }
+        }
+        // TODO: Questionnaire::qTemplate inputQuestionData{};
+};
+
+#pragma region InputImplementationPrototype
+    void collectUserInput(const string outputText, string &tempVariable) { // WIP
+        cout << outputText << ": " << endl;
+        getline(cin, tempVariable); // TODO: validate type
+    }
+#pragma endregion
+
+int main() {
+    QuizCreation qCreation;
+    qCreation.runQuizCreation();
 
     return 0;
 }
